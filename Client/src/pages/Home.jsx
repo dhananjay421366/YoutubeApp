36b928a98617d0d4af26de37d58a2f79d013285c
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2024-06-30 19:29:39 +0530

    next commite

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 export default function Home() {
   const [videos, setVideos] = useState([]);
-
+ 
 
   const fetchVideos = async () => {
     try {
@@ -239,7 +239,7 @@ export default function Home() {
           </aside>
           <section className="w-full pb-[70px] sm:ml-[70px] sm:pb-0 lg:ml-0">
             <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4 p-4">
-              {
+              {Array.isArray(videos) && videos.length > 0 ? (
                 videos.map((video) => (
                   <div className="w-full" key={video._id}>
                     <div className="relative mb-2 w-full pt-[56%]">
@@ -259,10 +259,10 @@ export default function Home() {
                       </div>
                     </div>
                   </div>
-                ))}
-              : (
-              <div className="text-center text-gray-400">No videos available</div>
-              )
+                ))
+              ) : (
+                <div className="text-center text-gray-400">No videos available</div>
+              )}
             </div>
           </section>
         </div >
